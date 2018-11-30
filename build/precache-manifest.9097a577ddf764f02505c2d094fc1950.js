bda08c38e597880c8806fba56083fd2000c56440
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-30 11:06:57 -0600

    Build for deployment

---
+++ build/precache-manifest.9097a577ddf764f02505c2d094fc1950.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0edd3da3b4e1066d5b9f",
+    "url": "/static/js/main.0edd3da3.chunk.js"
+  },
+  {
+    "revision": "2f67cb624ea3e9c62e54",
+    "url": "/static/js/1.2f67cb62.chunk.js"
+  },
+  {
+    "revision": "0edd3da3b4e1066d5b9f",
+    "url": "/static/css/main.240e5c2b.chunk.css"
+  },
+  {
+    "revision": "58db35e853f8915a2ff728d84f43dc1e",
+    "url": "/index.html"
+  }
+];
